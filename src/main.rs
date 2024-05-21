ab4e2508d1e1098a0f00f5cf28de16a337f8bf8c
Josh Andrews <joshurtree@gmail.com>
2024-05-21 20:40:42 +0100

    Update to use hyprland IPC.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
-use std::{env, fs, thread, time};
-use std::process::{Command, exit};
-use std::io::prelude::*;
-use serde::Deserialize;
-use serde_json;
+use std::{env, thread, time};
+use std::fs::{create_dir_all, File};
+use std::io::{read_to_string, Write};
 use std::path::PathBuf;
-use clap::Parser;
+use std::process::{Command, exit};
+//use serde::Deserialize;
+use clap::{Parser, ValueEnum};
 
+use hyprland::data::{Client, Clients};
+use hyprland::dispatch::*;
+//use hyprland::event_listener::EventListener;
+//use hyprland::keyword::*;
+use hyprland::prelude::*;
+//use hyprland::shared::WorkspaceType;
+
+/*
 #[derive(Debug, Deserialize)]
 struct WorkspaceInfo {
     id: i32,
     name: String
 }
+
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all="camelCase")]
@@ -36,24 +45,44 @@ struct ClientInfo {
     grouped: Vec<String>,
     swallowing: String,
 }
+*/
+
+#[derive(Copy, Clone, Parser, PartialEq, ValueEnum)]
+enum Mode {
+    /// Load session then periodicly save session (default)
+    Default,
+    
+    /// Periodicly save the session 
+    SaveOnly,
+    /// Save the session once then exit
+    SaveAndExit,
+
+    /// Load the session then exit
+    LoadAndExit
+}
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short='o', long)]
-    save_once: bool,
-
-    #[arg(short='i', long)]
-    save_immediately: bool,
-
+    /// Which mode to run the program in
     #[arg(short, long)]
-    save_interval: Option<u64>,
+    mode: Option<Mode>,
 
-    #[arg(short='p', long)]
+    /// Interval between saving sessions (default: 60)
+    #[arg(short='i', long)]
+    save_interval: Option<u64>,
+    
+    /// The path where the session is saved (default: ~/.local/share/session.conf)
+    #[arg(short='s', long)]
     session_path: Option<PathBuf>,
+
+    /// Only simulate calls to Hyprland (supresses loading of session)
+    #[arg(long)]
+    simulate: bool
 }   
 
-fn fetch_command(info: &ClientInfo) -> String {
+
+fn fetch_command(info: &Client) -> String {
     let output = Command::new("ps")
                     .arg("--no-headers")
                     .arg("-o")
@@ -71,64 +100,72 @@ fn run_if(prop: bool, val: &str) -> &str {
 
 fn save_session(session_path: &PathBuf) {
     let props = [ 
-        |info: &ClientInfo| format!("monitor {}", info.monitor),
-        |info: &ClientInfo| format!("workspace {} silent", info.workspace.id), 
-        |info: &ClientInfo| format!("{}", run_if(info.floating, "float")),
-        |info: &ClientInfo| format!("move {} {}", info.at.0, info.at.1),  
-        |info: &ClientInfo| format!("size {} {}", info.size.0, info.size.1), 
-        |info: &ClientInfo| format!("{}", run_if(info.pinned, "pin")), 
-        |info: &ClientInfo| format!("{}", run_if(info.fullscreen, "fullscreen")), 
-        |info: &ClientInfo| format!("{}", run_if(info.fake_fullscreen, "fakefullscreen")) 
+        |info: &Client| format!("monitor {}", info.monitor),
+        |info: &Client| format!("workspace {} silent", info.workspace.id), 
+        |info: &Client| format!("{}", run_if(info.floating, "float")),
+        |info: &Client| format!("move {} {}", info.at.0, info.at.1),  
+        |info: &Client| format!("size {} {}", info.size.0, info.size.1), 
+        |info: &Client| format!("{}", run_if(info.pinned, "pin")), 
+        |info: &Client| format!("{}", run_if(info.fullscreen, "fullscreen")), 
+        //|info: &Client| format!("{}", run_if(info.fake_fullscreen, "fakefullscreen")) 
     ];
 
-    let output = Command::new("hyprctl")
-                    .arg("-j")
-                    .arg("clients")
-                    .output()
-                    .expect("Failed to call hyprctl");
-
-    let client_info: Vec<ClientInfo> = serde_json::from_str(&String::from_utf8_lossy(&output.stdout).to_string()).unwrap();
-
-    let mut session_config = fs::File::create(session_path)
-                                .expect("Failed to create session file");
+    let client_info = Clients::get().expect("Unable to fetch clients");
     
-    for info in &client_info {
+    let mut session_config = File::create(session_path)
+                                .expect("Failed to create session file");
+    for info in client_info.iter() {
         let exec_opts: Vec<String> = 
             props
                 .iter()
                 .map(|opt| opt(info))
                 .filter(|opt| !opt.is_empty())
                 .collect();
-        session_config.write(format!("exec-once = [{}] {}", exec_opts.join(";"), fetch_command(info)).as_bytes());
+        session_config.write(format!("[{}] {}", exec_opts.join(";"), fetch_command(info)).as_bytes());
     }
     println!("Session saved");
 }
 
+
+fn load_session(session_path: &PathBuf, simulate: bool) {
+    let session_file = File::open(session_path).expect("Failed to open session file.");
+    
+    for line in read_to_string(session_file).unwrap().lines() {
+        if !simulate {
+            hyprland::dispatch!(Exec, line);
+        } 
+        println!("Sending: dispach exec {}", line);
+    }
+}
+
 fn main() {
     let args = Args::parse();
+    let mode = args.mode.unwrap_or(Mode::Default);
     let save_interval = args.save_interval.unwrap_or(60);
+    let simulate = args.simulate;
+    let share_dir = env::var("HOME").unwrap() + "/.local/share/hyprsession";
+
     if save_interval < 1 {
         panic!("Save interval needs to be a positive integer");
     }
 
-    let share_dir = env::var("HOME").unwrap() + "/.local/share/hyprsession";
-    fs::create_dir_all(&share_dir).expect("Failed to create share dir");
+    create_dir_all(&share_dir).expect("Failed to create share dir");
     let default_path = PathBuf::from(share_dir + "/session.conf");
     let session_path = args.session_path.unwrap_or(default_path);
 
-    fs::File::create(&session_path)
-        .expect("Failed to create session file");
+    match mode {
+        Mode::Default | Mode::LoadAndExit =>
+            load_session(&session_path, simulate),
+        Mode::SaveAndExit | Mode::SaveOnly => save_session(&session_path)
+    } 
 
-    if args.save_once || args.save_immediately || cfg!(debug_assertions) {
-        save_session(&session_path);
-        if args.save_once { 
-            exit(0);
-        }
+    if mode == Mode::LoadAndExit || mode == Mode::SaveAndExit {
+        exit(0);
     }
 
     loop {
-        thread::sleep(time::Duration::from_secs(save_interval));
         save_session(&session_path);
+        thread::sleep(time::Duration::from_secs(save_interval));
     }
 }
     
