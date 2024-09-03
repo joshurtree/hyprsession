a5e6b4dae671480be98e03a197346f3b9c9b4b5c
tiecia <ty.cia@outlook.com>
2024-09-02 19:31:11 -0700

    cleanup flake and remove default.nix

--- default.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> { } }:
-pkgs.rustPlatform.buildRustPackage rec {
-  pname = "hyprsession";
-  version = "0.1.3";
-  cargoLock.lockFile = ./Cargo.lock;
-  src = pkgs.lib.cleanSource ./.;
-}
--- flake.nix
+++ flake.nix
@@ -2,15 +2,23 @@
   description = "Hyprsession: A session saver for Hyprland";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgsFor = nixpkgs.legacyPackages;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
     in {
-      packages = forAllSystems (system: {
-        default = pkgsFor.${system}.callPackage ./. { };
-      });
-    };
+      packages = {
+        default = pkgs.rustPlatform.buildRustPackage {
+          pname = "hyprsession";
+          version = "0.1.4";
+          cargoLock.lockFile = ./Cargo.lock;
+          src = pkgs.lib.cleanSource ./.;
+        };
+      };
+    });
 }
